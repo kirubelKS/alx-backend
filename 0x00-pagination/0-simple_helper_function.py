cfb79c2780fbdc807bf5a19330da3d41c62fd176
kirubelks <kirubel.n.gebeyehu@gmail.com>
2023-10-22 16:38:01 +0300

    pagination project commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Simple helper function.'''
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''Retrieves the index range from a given page amd page size.'''
+    start = (page -1) * page_size
+    end = start + page_size 
+    return (start, end)
